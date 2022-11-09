2491115b38a8778e54e66609ed0626e7e5774296
ibrahim6755 <ibrahimghani020@gmail.com>
2022-11-09 08:10:33 +0000

    changes

--- service details.js
+++ service details.js
@@ -1,9 +1,6 @@
 $(document).ready(function(){
-    $('#btn').on('mouseover',function(){
-     $('#box').hide(3000);
-    })
-    $('#btn').on('mouseleave',function(){
-     $('#box').show(3000);
+    $('#btn').on('click',function(){
+     $('#box').toggle(3000);
     })
     
  })
