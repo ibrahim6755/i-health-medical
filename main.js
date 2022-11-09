24759a7f52ab513182517269d01ef6de61bef032
ibrahim6755 <ibrahimghani020@gmail.com>
2022-11-09 19:22:14 +0000

    changes

--- main.js
+++ main.js
@@ -2,4 +2,7 @@ $(document).ready(function(){
     $('#read-more').on('click',function(){
      $('#expand-para').toggle();
     })
+    $('#appointment-btn').on('click',function(){
+        $('#appointment').toggle();
+       })
  })
