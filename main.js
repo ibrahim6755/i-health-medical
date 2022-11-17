319b30d63d30c2f2d17411419c3ff156c8337fc2
ibrahim6755 <ibrahimghani020@gmail.com>
2022-11-17 11:19:35 +0000

    changes

--- main.js
+++ main.js
@@ -2,4 +2,30 @@ $(document).ready(function(){
     $('#read-more').on('click',function(){
      $('#expand-para').toggle();
     })
+
+
  })
+
+ $(document).ready(function(){
+    $('#make').on('click' , function(){
+        let name = $('#name').val();
+        let email = $('#email').val();
+        let subject = $('#subject').val();
+        let msg = $('#msg').val();
+    
+        let person = {
+            name:name,
+            email:email,
+            subject:subject,
+            msg:msg
+        }
+        localStorage.setItem('data' , JSON.stringify(person))
+         $('#name').val('');
+         $('#email').val('');
+         $('#subject').val('');
+         $('#msg').val('');
+    })
+
+ })
+
+  
